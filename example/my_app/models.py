b59387d759fba3cb2cfe738a5574b51180e48234
Andrii <andrii.pidlisnyi@gmail.com>
2018-04-25 15:07:28 +0300

    add priority field to Project model of example project

--- example/my_app/models.py
+++ example/my_app/models.py
@@ -7,13 +7,24 @@ class Project(models.Model):
     IN_PROGRESS = 2
     DONE = 3
 
+    LOW = 1
+    AVERAGE = 2
+    HIGH = 3
+
     STATUS_CHOICE = (
         (NEW, 'New'),
         (IN_PROGRESS, 'In Progress'),
         (DONE, 'Done')
     )
 
+    PRIORITY_CHOICE = (
+        (LOW, 'Low'),
+        (AVERAGE, 'Average'),
+        (HIGH, 'High')
+    )
+
     name = models.CharField(max_length=100)
-    status = models.IntegerField(choices=STATUS_CHOICE)
+    status = models.IntegerField(choices=STATUS_CHOICE, default=NEW)
+    priority = models.IntegerField(choices=PRIORITY_CHOICE, default=LOW)
     description = models.TextField()
     team_lead_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
