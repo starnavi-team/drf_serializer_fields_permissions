d06e374c25788ac662bab7c793bf222b074ca7dc
Monstradamus <bohdan.y@starnavi.io>
2024-01-18 11:28:41 +0200

    Fix iteration in utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -10,14 +10,21 @@ class BaseTestingClass(APITestCase):
     def setUp(self):
         password = 'qwerty123'
         self.team_lead = User.objects.create(
-            username='team_lead', password=make_password(password), is_superuser=True, is_staff=True
+            username='team_lead',
+            password=make_password(password),
+            is_superuser=True, is_staff=True
         )
         self.team_member = User.objects.create(
-            username='team_member', password=make_password(password), is_staff=True
+            username='team_member',
+            password=make_password(password),
+            is_staff=True
         )
         self.customer = User.objects.create(
             username='customer', password=make_password(password)
         )
         self.project = Project.objects.create(
-            name='Test Project', status=1, description='Project description', team_lead_user=self.team_lead
+            name='Test Project',
+            status=1,
+            description='Project description',
+            team_lead_user=self.team_lead
         )
