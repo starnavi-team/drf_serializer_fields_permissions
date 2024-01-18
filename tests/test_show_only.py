856b4f4f530218f130d77a7129eb544704e00227
Monstradamus <bohdan.y@starnavi.io>
2024-01-18 11:29:11 +0200

    Fix iteration in test files

--- tests/test_show_only.py
+++ tests/test_show_only.py
@@ -9,24 +9,33 @@ class WriteOnlyFieldPermissionTest(BaseTestingClass):
 
     def test_edit_status_field_for_team_lead(self):
         self.client.force_login(user=self.team_lead)
-        response = self.client.patch(reverse('project-detail', kwargs={'pk': self.project.pk}),
-                                     data={'status': 2})
+        response = self.client.patch(reverse(
+            'project-detail',
+            kwargs={'pk': self.project.pk}),
+            data={'status': 2}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         project = Project.objects.get(id=1)
         self.assertEqual(project.status, 2)
 
     def test_edit_status_field_for_team_member(self):
         self.client.force_login(user=self.team_member)
-        response = self.client.patch(reverse('project-detail', kwargs={'pk': self.project.pk}),
-                                     data={'status': 2})
+        response = self.client.patch(reverse(
+            'project-detail',
+            kwargs={'pk': self.project.pk}),
+            data={'status': 2}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         project = Project.objects.get(id=1)
         self.assertEqual(project.status, 2)
 
     def test_edit_status_field_for_customer(self):
         self.client.force_login(user=self.customer)
-        response = self.client.patch(reverse('project-detail', kwargs={'pk': self.project.pk}),
-                                     data={'status': 2})
+        response = self.client.patch(reverse(
+            'project-detail',
+            kwargs={'pk': self.project.pk}),
+            data={'status': 2}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         project = Project.objects.get(id=1)
         # But the field was not changed
